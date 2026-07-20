4246f2798453a4a355d1adbcd54d7a82f8a1cab9
RangS-1 <rangga19sj@gmail.com>
2026-07-20 17:54:38 +0700

    Update: Basic Command features

--- src/ferminal/main.py
+++ src/ferminal/main.py
@@ -17,14 +17,12 @@ def main():
         except Exception as e: # i thought exception is just a var -_- 
             raise e
         
-        #command
+        #-- Basic Commands!
         if command.strip().startswith("x"):
             print("Bye -_-\n")
             sys.exit(1)
-
         elif command.strip().startswith("b"):
-            os.system('cls')
-        
+            os.system('clear')
         elif command.strip().lower().startswith("e"):
             if len(insert) > 1:
                 print("\n ".join(insert[1:]))
@@ -53,42 +51,41 @@ def main():
         elif command.strip().lower() == "w":
             wd = os.getcwd() #working directory
             print(wd,"\n")
-        
         elif command[:2] == "d ":
             if not os.path.exists(command[2:]):
-                print("Directory not found bruh :^\n")
+                print("Directory not found!\n")
             else:
                 os.chdir(command[2:])
-        
         elif command[:2] == "k ":
-            os.mkdir(command[2:])
-
+            folder = os.mkdir(command[2:])
+            print(f"Folder called {command[2:]} has been made")
         elif command[:2] == "m ":
-            os.rmdir(command[2:])
-            print(f"folder has been deleted :^\n")
-
-        elif command[:2] == "n ":
-            os.system(f'del {command[2:]}')
-            print(f"file has been deleted :^\n")
-            
+            if not os.path.exists(command[2:]):
+                print("Directory not found!\n")
+            else:
+                os.rmdir(command[2:])
+                print(f"Folder called {command[2:]} has been deleted")
+        elif command[:2] == "r ":
+            folder = os.system(f'rm {command[2:]}')
+            print(f"File called {command[2:]} deleted!\n")
         elif command.strip().lower() == "l":
-            lis = os.system('dir')
-            print(lis)
-
+            lis = os.system('ls')
+        elif command.strip().lower() == "la":
+            lis = os.system('ls -a')
         elif command[:2] == "f ":
-            file = os.system(f'type nul > {command[2:]}')
-            print(f"file called {command[2:]} is made")
-        
+            file = os.system(f'touch {command[2:]}')
+            print(f"File called {command[2:]} is made")
         elif command[:2] == "v ":
-            move = os.system(f'move {command[2:]}')
-            print(f"file called {command[2:]} is moved")
-
+            move = os.system(f'mv {command[2:]}')
+            print(f"File or Folder called {command[2:]} is moved")
         elif command[:2] == "c ":
-            copy = os.system(f'copy {command[2:]}')
-            print(f"file called {command[2:]} is moved")
-
+            copy = os.system(f'cp {command[2:]}')
+            print(f"File called {command[2:]} has been copied and moved")
+        elif command[:3] == "cr ":
+            copy = os.system(f'cp -r {command[3:]}')
+            print(f"Folder called {copy} has been copied and moved")
         elif command[:2] == "u ":
-            u = os.system(f'type {command[2:]}')
+            u = os.system(f'cat {command[2:]}')
             print(f"\n\ninside of {command[2:]}")
 
         elif command == 'z': # i'm just trolling _:), at least you can rest for 30 seconds :v
@@ -123,55 +120,41 @@ def main():
             else:
                 print("Read Documentation!")
 
-        # GIT
+        #-- Git
         elif command[:3] == "gia":
             git_add = os.system(f'git add {command[4:]}')
-            print("You have added the file to the staging area!")
         elif command[:3] == "gis":
             git_stat = os.system(f'git status "{command[6:]}"')
-            print("That's the Git Status!")
         elif command[:3] == "gic":
             git_clone = os.system(f'git clone {command[3:]}')
-            print("You have cloned the repository!")
         elif command[:3] == "gin":
             git_init = os.system('git init')
-            print("You have initialized the repository!")
         elif command[:3] == "gib":
             git_branch = os.system(f'git branch {command[3:]}')
-            print("You have created the branch!")
         elif command[:3] == "gibd":
             git_branch = os.system(f'git branch -d {command[3:]}')
-            print("You have deleted the branch!")
         elif command[:3] == "gip":
             git_pull = os.system(f'git pull {command[3:]}')
-            print("You have pulled the repository!")
         elif command[:3] == "gih":
             git_push = os.system(f'git push {command[3:]}')
-            print("You have pushed the repository!")
         elif command[:3] == "gim":
             git_commit = os.system(f'git commit -m "{command[3:]}"')
-            print("You have committed the repository!")
         elif command[:3] == 'gil':
             log = os.system("git log")
         elif command[:3] == 'gif':
             git_fetch = os.system(f'git fetch {command[6:]}')
-            print("You have fetched the repository!")
         elif command[:3] == 'gir':
             git_remote = os.system(f'git remote {command[6:]}')
         elif command[:3] == 'gie':
             git_merge = os.system(f'git merge {command[3:]}')
         elif command[:3] == 'gio':
             git_checkout = os.system(f'git checkout {command[6:]}')
-            print("You have checked out the branch!")
         elif command[:3] == 'giob':
             git_checkout = os.system(f'git checkout -b {command[6:]}')
-            print("You have created and checked out the branch!")
         elif command[:3] == 'gig':
             git_tag = os.system(f'git tag {command[3:]}')
-            print("You have created the tag!")
         else:
             os.system(command)
-            print("Done using CMD _:)\n")
 
 
 if __name__ == '__main__':
